7116a70231872d9651b34dac0f645b7d5fed6018
geoffn7 <geoffrey.nguyen9@gmail.com>
2020-07-19 03:28:19 -0400

    Symbols can now be included, but are not processed. README updated and docstrings added.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Vigenere-Cipher
-Implementation of the Vigenere Cipher
+Implementation of the Vigenere Cipher.
+
+## Usage
+
+```
+python vigenere_cipher  <message>   <key>   --encode #to encode a message
+python vigenere_cipher  <cipher>   <key>   --decode #to decode a message
+python vigenere_cipher  --test #to run through test cases
+```
+
+## Examples
+```
+python vigenere_cipher "Attack at Dawn!" "pie" --encode #prints cipher: "Pbxpko pb Hper!"
+python vigenere_cipher "Pbxpko pb Hper!" "pie" --decode #prints the original message: "Attack at Dawn!"
+```
--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -1,13 +1,32 @@
+"""Vigenere Cipher Encoder and Decoder
+
+This script allows the user to:
+
+    * encrypt a message using a key
+    * decrypt a cipher using the key used to encrypt the original message
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * num_to_alpha - converts an integer to an alphabetical letter
+    * alpha_to_num - converts an alphabetical letter to an integer
+    * generate_cipher_matrix - returns the matrix used for the vigenere cipher
+    * encode - encodes a message using the inputted key
+    * decode - decodes a cipher with the inputted key
+    * manager - handles encoding and decoding
+    * test - runs test cases for vigenere cipher implemented in this module
+"""
+
 import sys
 from collections import deque
 
-def num_to_alpha(num):
+def num_to_alpha(num: int) -> str:
     return chr(num+96)
 
-def alpha_to_num(alpha):
+def alpha_to_num(alpha: str) -> int:
     return ord(alpha)-96
 
-def generate_cipher_matrix():
+def generate_cipher_matrix() -> list:
     cipher_matrix = [deque([x for x in range(1, 27)]) for y in range(1, 27)]
     row = 0
     while row < 26:
@@ -15,10 +34,10 @@ def generate_cipher_matrix():
         row+=1
     return cipher_matrix
 
-def get_keystream(key, message_len):
+def get_keystream(key: str, msg_len: int) -> str:
     keystream = ""
     key_len = len(key)
-    quotient, remainder = divmod(message_len, key_len)
+    quotient, remainder = divmod(msg_len, key_len)
     times = 0
     while times < quotient:
         keystream+=key
@@ -26,7 +45,7 @@ def get_keystream(key, message_len):
     keystream+=key[0:remainder]
     return keystream
 
-def encode(msg, key):
+def encode(msg: list, key: str) -> list:
     matrix = generate_cipher_matrix()
     keystream = get_keystream(key, len(msg))
 
@@ -40,14 +59,13 @@ def encode(msg, key):
         item+=1
     return cipher
 
-def decode(cipher, key):
+def decode(cipher: list, key: str) -> list:
     matrix = generate_cipher_matrix()
     keystream = get_keystream(key, len(cipher))
 
     msg = []
     item = 0
     while item < len(cipher):
-        #for each letter
         row = 0
         while row < 26:
             if num_to_alpha(matrix[row][alpha_to_num(keystream[item])-1]) == cipher[item]:
@@ -56,7 +74,7 @@ def decode(cipher, key):
         item += 1
     return msg
 
-def manager(msg, key, option):
+def manager(msg: str, key: str, option: str) -> str:
 
     #Converting string to list to work with
     msg = list(msg)
@@ -70,65 +88,119 @@ def manager(msg, key, option):
             msg[letter_index] = msg[letter_index].lower()
         letter_index+=1
 
-    # Dealing with spaces and symbols and non-alphanumeric input
-    # Create temporary msg without all of the spaces and symbols
-    # Add the alphanumeric values back in once done
+    #Handling spaces and other non-alphanumeric input
+    original_msg = msg
+    alpha_msg = []
+    for item in original_msg:
+        if item.isalnum():
+            alpha_msg.append(item)
 
     if option == "--encode":
-        result = encode(msg, key)
+        result = encode(alpha_msg, key)
 
     if option == "--decode":
-        result = decode(msg, key)
+        result = decode(alpha_msg, key)
 
+    #Handling Spaces and Symbols
+    final_result = []
+    for item in original_msg:
+        if item.isalpha():
+            final_result.append(result.pop(0))
+        else:
+            final_result.append(item)
+
+    #Handling Capital Letters
     for letter in capital_letters:
-        result[letter] = result[letter].upper()
-    return "".join(result)
+        final_result[letter] = final_result[letter].upper()
+
+    return "".join(final_result)
 
 def test():
-    # Test Case 1 (simple encode)
+    """Test function for vigenere cipher. Goes through several test cases."""
+
+    print("\nTest Case #1: Simple Encode")
     try:
         cipher = manager("attackatdawn", "pie", "--encode")
-        if cipher == "pbxpkopbhper":
-            print("Test Case 1: Pass")
+        expected_cipher = "pbxpkopbhper"
+        if cipher == expected_cipher:
+            print("Result: Pass")
         else:
-            print("Test Case 1: Fail")
+            print("Result: Fail")
+            print("Cipher: "+cipher)
+            print("Expected Cipher: "+expected_cipher)
     except Exception as error:
-        print("Test Case 1: Fail - "+ str(error))
+        print("Error - "+ str(error))
 
-    # Test Case 2 (simple decode)
+    #--------------------------------------------------------------------------#
+    print("\nTest Case #2: Simple Decode")
     try:
         msg = manager("pbxpkopbhper", "pie", "--decode")
-        if msg == "attackatdawn":
-            print("Test Case 2: Pass")
+        expected_msg = "attackatdawn"
+        if msg == expected_msg:
+            print("Result: Pass")
         else:
-            print("Test Case 2: Fail")
+            print("Result: Fail")
+            print("Message: "+msg)
+            print("Expected Message: " +expected_msg)
     except Exception as error:
-        print("Test Case 2: Fail - "+ str(error))
+        print("Error - "+ str(error))
 
-    # Test Case 3 (encode with capital letters)
+    #--------------------------------------------------------------------------#
+    print("\nTest Case #3: Message with Capital Letters")
     try:
         cipher = manager("AttackAtDawn", "pie", "--encode")
-        if cipher == "PbxpkoPbHper":
-            print("Test Case 3: Pass")
+        expected_cipher = "PbxpkoPbHper"
+        if cipher == expected_cipher:
+            print("Result: Pass")
         else:
-            print("Test Case 3: Fail")
+            print("Result: Fail")
+            print("Cipher: "+cipher)
+            print("Expected Cipher: "+expected_cipher)
     except Exception as error:
-        print("Test Case 3: Fail - "+ str(error))
+        print("Error - "+ str(error))
 
-    # Test Case 4 (encode with symbols)
-
-    # Test Case 5 (decode with capital letters)
+    #--------------------------------------------------------------------------#
+    print("\nTest Case #4: Cipher with Capital Letters")
+    # Test Case 4 (decode with capital letters)
     try:
         msg = manager("PbxpkoPbHper", "pie", "--decode")
-        if msg == "AttackAtDawn":
-            print("Test Case 4: Pass")
+        expected_msg = "AttackAtDawn"
+        if msg ==expected_msg:
+            print("Result: Pass")
         else:
-            print("Test Case 4: Fail")
+            print("Result: Fail")
+            print("Message: "+msg)
+            print("Expected Message: " +expected_msg)
     except Exception as error:
-        print("Test Case 4: Fail - "+ str(error))
+        print("Error - "+ str(error))
 
+    #--------------------------------------------------------------------------#
+    print("\nTest Case #5: Message with Symbols, Spaces and Special Characters")
+    try:
+        cipher = manager("Attack At Dawn!", "pie", "--encode")
+        expected_cipher = "Pbxpko Pb Hper!"
+        if cipher == expected_cipher:
+            print("Result: Pass")
+        else:
+            print("Result: Fail")
+            print("Cipher: "+cipher)
+            print("Expected Cipher: "+expected_cipher)
+    except Exception as error:
+        print("Error - "+ str(error))
 
-    # Test Case 6 (decode with symbols)
+    #--------------------------------------------------------------------------#
+    print("\nTest Case #6: Cipher with Symbols, Spaces and Special Characters")
+    try:
+        cipher = manager("Pbxpko Pb Hper!", "pie", "--decode")
+        expected_msg = "Attack At Dawn!"
+        if cipher == expected_msg:
+            print("Result: Pass")
+        else:
+            print("Result: Fail")
+            print("Message: "+msg)
+            print("Expected Message: " +expected_msg)
+    except Exception as error:
+        print("Error - "+ str(error))
 
 if __name__ == "__main__":
 
@@ -137,11 +209,17 @@ if __name__ == "__main__":
             if sys.argv[1] == "--test":
                 test()
                 sys.exit()
-        print("Help me!")
+        print("\n   Usage: \n")
+        print("     python vigenere_cipher  <message>   <key>   --encode")
+        print("     python vigenere_cipher  <cipher>   <key>   --decode")
+        print("     python vigenere_cipher  --test\n")
         sys.exit()
 
     elif len(sys.argv) > 3:
         msg = sys.argv[1]
         key = sys.argv[2]
         option = sys.argv[3]
+        if len(key) > len(msg):
+            print("Error - key cannot be longer than message or cipher")
+            sys.exit()
         print(manager(msg, key, option))
